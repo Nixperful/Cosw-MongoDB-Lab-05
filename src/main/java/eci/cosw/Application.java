42ff688cf095c00106eda9b6b5f718c355d2cfb3
nixperful <nicolasosorioarias@hotmail.com>
2018-09-20 22:32:56 -0500

    Puntos Finalizados

--- src/main/java/eci/cosw/Application.java
+++ src/main/java/eci/cosw/Application.java
@@ -14,6 +14,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+
+import java.util.List;
 
 @SpringBootApplication
 public class Application implements CommandLineRunner {
@@ -64,7 +68,7 @@ public class Application implements CommandLineRunner {
         userRepository.save(new User("Joao" , "joao@test.com" ));
         todoRepository.save(new Todo("Travel to Irland", "3", "Sep 21 - 2019", "joao@test.com", "I have Tickets" ));
         todoRepository.save(new Todo("Cook turkey", "2", "Nov 19 - 2018", "joao@test.com", "I buy the Turkey" ));
-        todoRepository.save(new Todo("COSW proyect", "1", "Oct 22 - 2018", "joao@test.com", "Begining" ));
+        todoRepository.save(new Todo("Work proyect", "1", "Oct 22 - 2018", "joao@test.com", "Begining" ));
 
         userRepository.save(new User("Adriana" , "adriana@test.com" ));
         todoRepository.save(new Todo("Friday Party", "3", "Sep 21 - 2018", "adriana@test.com", "I have the clothes" ));
@@ -83,22 +87,37 @@ public class Application implements CommandLineRunner {
         todoRepository.save(new Todo("Paying Taxes", "1", "Jul 21 - 2018", "david@test.com", "Completed" ));
         todoRepository.save(new Todo("Wash Car", "3", "May 30 - 2018", "david@test.com", "Completed" ));
 
-        userRepository.save(new User("Nicolas" , "nicolas@test.com" ));
-        todoRepository.save(new Todo("Paying Taxes", "1", "Jul 21 - 2018", "david@test.com", "Completed" ));
-        todoRepository.save(new Todo("Wash Car", "3", "May 30 - 2018", "david@test.com", "Completed" ));
+        userRepository.save(new User("Daniela" , "daniela@test.com" ));
+        todoRepository.save(new Todo("Guitar Class", "2", "Jul 02 - 2018", "daniela@test.com", "Assisted" ));
+        todoRepository.save(new Todo("Auditions", "1", "Nov 30 - 2018", "daniela@test.com", "practicing" ));
 
-        userRepository.save(new User("Nicolas" , "nicolas@test.com" ));
-        todoRepository.save(new Todo("Paying Taxes", "1", "Jul 21 - 2018", "david@test.com", "Completed" ));
-        todoRepository.save(new Todo("Wash Car", "3", "May 30 - 2018", "david@test.com", "Completed" ));
+        userRepository.save(new User("Jose" , "jose@test.com" ));
+        todoRepository.save(new Todo("Soccer Tournament", "3", "Jul 22 - 2018", "jose@test.com", "3rd place" ));
+        todoRepository.save(new Todo("Go to medic", "3", "Oct 04 - 2018", "jose@test.com", "Pending" ));
 
-        userRepository.save(new User("Nicolas" , "nicolas@test.com" ));
-        todoRepository.save(new Todo("Wash Car", "3", "May 30 - 2018", "david@test.com", "Completed" ));
-        todoRepository.save(new Todo("Wash Car", "3", "May 30 - 2018", "david@test.com", "Completed" ));
+        userRepository.save(new User("Sara" , "sara@test.com" ));
+        todoRepository.save(new Todo("operate the patient", "1", "May 21 - 2018", "sara@test.com", "Completed" ));
+        todoRepository.save(new Todo("tennis match", "3", "Nov 20 - 2018", "sara@test.com", "Completed" ));
+        todoRepository.save(new Todo("go to Monserrate", "2", "Jul 21 - 2018", "carlos@test.com", "Done" ));
 
-        userRepository.save(new User("Nicolas" , "nicolas@test.com" ));
-        todoRepository.save(new Todo("Paying Taxes", "1", "Jul 21 - 2018", "david@test.com", "Completed" ));
-        todoRepository.save(new Todo("Wash Car", "3", "May 30 - 2018", "david@test.com", "Completed" ));
-        todoRepository.save(new Todo("Paying Taxes", "1", "Jul 21 - 2018", "david@test.com", "Completed" ));
+        userRepository.save(new User("Carlos" , "carlos@test.com" ));
+        todoRepository.save(new Todo("Draw the Mountain", "1", "Jul 21 - 2018", "carlos@test.com", "Completed" ));
+        todoRepository.save(new Todo("Buy drinks for party", "2", "Sep 03 - 2018", "carlos@test.com", "Completed" ));
+        todoRepository.save(new Todo("Go to karaoke with friends", "3", "Sep 22 - 2018", "carlos@test.com", "Pending" ));
+
+
+        Query query2 = new Query();
+        query2.addCriteria(Criteria.where("priority").gt(5));
+
+        Todo todos = mongoOperation.findOne(query2, Todo.class);
+
+
+
+        Query query1 = new Query();
+        query1.addCriteria(Criteria.where("description").gt(10));
+
+        Customer customer = mongoOperation.findOne(query1, Customer.class);
+
         
     }
 
